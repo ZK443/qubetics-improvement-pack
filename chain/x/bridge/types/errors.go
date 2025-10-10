798603fe6f38b5e2870b955d913aa5fdeba51b4a
ZK443 <zakaikaido@gmail.com>
2025-10-10 10:46:59 +0200

    errors.go

--- chain/x/bridge/types/errors.go
+++ chain/x/bridge/types/errors.go
@@ -3,6 +3,11 @@ package types
 import "cosmossdk.io/errors"
 
 var (
-    ErrProofAlreadyExists = errors.Register(ModuleName, 1100, "proof already exists")
-    ErrAlreadyExecuted    = errors.Register(ModuleName, 1101, "message already executed")
+    ErrUnauthorized      = errors.Register(ModuleName, 1200, "unauthorized")
+    ErrInvalidProof      = errors.Register(ModuleName, 1201, "invalid proof")
+    ErrNotVerified       = errors.Register(ModuleName, 1202, "proof not verified")
+    ErrProofAlreadyExists= errors.Register(ModuleName, 1203, "proof already exists")
+    ErrAlreadyExecuted   = errors.Register(ModuleName, 1204, "already executed")
+    ErrBridgePaused      = errors.Register(ModuleName, 1205, "bridge paused")
+    ErrRateLimitExceeded = errors.Register(ModuleName, 1206, "rate limit exceeded")
 )
