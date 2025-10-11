7d9ff57b9fe36dfc2708b71ff505d7763be8c535
ZK443 <zakaikaido@gmail.com>
2025-10-11 18:51:29 +0200

    main.go

--- chain/cmd/qubeticsd/main.go
+++ chain/cmd/qubeticsd/main.go
@@ -17,12 +17,14 @@ func main() {
 	}
 
 	// tx namespace
-	txCmd := &cobra.Command{
-		Use:   "tx",
-		Short: "Transactions subcommands",
-	}
+	txCmd := &cobra.Command{Use: "tx", Short: "Transactions subcommands"}
 	txCmd.AddCommand(bridgecli.NewTxCmd())
 	root.AddCommand(txCmd)
+
+	// query namespace
+	qCmd := &cobra.Command{Use: "query", Short: "Query subcommands"}
+	qCmd.AddCommand(bridgecli.NewQueryCmd())
+	root.AddCommand(qCmd)
 
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
