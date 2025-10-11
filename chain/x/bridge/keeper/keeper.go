756e313746b189309b1cb12af1a129ae420da98c
ZK443 <zakaikaido@gmail.com>
2025-10-11 15:22:21 +0200

    keeper.go

--- chain/x/bridge/keeper/keeper.go
+++ chain/x/bridge/keeper/keeper.go
@@ -1,9 +1,10 @@
+//go:build !cosmos
+
 package keeper
 
 import "github.com/ZK443/qubetics-improvement-pack/chain/x/bridge/types"
 
-// Лёгкий in-memory Keeper для прототипа и тестов CI.
-// Не тянем Cosmos SDK, чтобы держать сборку быстрой и простой.
+// Лёгкий in-memory Keeper для прототипа и CI.
 type Keeper struct {
 	paused   bool
 	executed map[string]bool
@@ -17,9 +18,9 @@ func NewKeeper() *Keeper {
 	}
 }
 
-func (k *Keeper) isPaused() bool                            { return k.paused }
-func (k *Keeper) getStatusByID(id string) types.Status      { return k.status[id] }
-func (k *Keeper) isExecuted(id string) bool                 { return k.executed[id] }
+func (k *Keeper) isPaused() bool                          { return k.paused }
+func (k *Keeper) getStatusByID(id string) types.Status    { return k.status[id] }
+func (k *Keeper) isExecuted(id string) bool               { return k.executed[id] }
 func (k *Keeper) rateLimited(_ types.MsgExecute) (bool, string) { return false, "" }
-func (k *Keeper) markExecuted(id string)                    { k.executed[id] = true }
-func (k *Keeper) emitEvent(_ string, _ map[string]string)   {}
+func (k *Keeper) markExecuted(id string)                  { k.executed[id] = true }
+func (k *Keeper) emitEvent(_ string, _ map[string]string) {}
